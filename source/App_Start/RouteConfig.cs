bb0582f09f34bc3e68b513f954d628da86d743ff
Son Nguyen <everestt2806@gmail.com>
2024-11-25 20:46:36 +0700

    Add files via upload

---
+++ source/App_Start/RouteConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace FashionStore
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // Route cho HomeController trong không gian tên chính
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "FashionStore.Controllers" } // Thêm không gian tên
+            );
+
+            // Route cho HomeController trong khu vực Admin
+            routes.MapRoute(
+                name: "Admin",
+                url: "Admin/{controller}/{action}/{id}",
+                defaults: new { action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "FashionStore.Areas.Admin.Controllers" } // Thêm không gian tên
+            );
+        }
+    }
+}
